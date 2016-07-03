fcc28df448e1c5a8bb3bde0f130a86f0cce7165f
Xiamin <610315802@qq.com>
2016-07-03 22:03:57 +0800

    加上特效！！！duangduang

--- app/src/main/java/com/example/xiamin/myapplication/MainActivity.java
+++ app/src/main/java/com/example/xiamin/myapplication/MainActivity.java
@@ -1,15 +1,12 @@
 package com.example.xiamin.myapplication;
 
-import android.content.Context;
 import android.content.Intent;
-import android.net.Uri;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -43,8 +40,12 @@ public class MainActivity extends AppCompatActivity {
                 *intent 第一个参数 上下文对象
                 *       第二个参数 目标文件
                 */
+
+
                 Intent intent = new Intent(MainActivity.this, controlActivity.class);
-                MainActivity.this.startActivityForResult(intent, 1);
+
+                startActivity(intent);
+                overridePendingTransition(R.anim.activityin,R.anim.activityout);
 
             }
         });
--- app/src/main/java/com/example/xiamin/myapplication/controlActivity.java
+++ app/src/main/java/com/example/xiamin/myapplication/controlActivity.java
@@ -1,37 +1,71 @@
 package com.example.xiamin.myapplication;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Xiamin on 2016/6/21.
  */
 public class controlActivity extends AppCompatActivity {
-    private Button button2;
+    private ListView listView;
+    private ScrollView scrollView;
+    private SimpleAdapter simpleAdapter;
+    private List<Map<String,Object>> datalist;
+    private Button returnButton = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.userinfo);
 
-        String[] arryydata = new String[]{"we","dsf"};
 
-        button2 = (Button)findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent data = new Intent();
-                data.putExtra("data","hello");
-                setResult(2,data);
 
-                //结束当前页面
-                finish();
-            }
-        });
+        ItemInit();
+        listTest();
+
+    }
+
+    private void ItemInit()
+    {
+        listView = (ListView) findViewById(R.id.listview);
+        scrollView = (ScrollView) findViewById(R.id.myscrollView);
+        datalist = new ArrayList<Map<String, Object>>();
+
+        simpleAdapter = new SimpleAdapter(controlActivity.this,datalist,R.layout.useritem,
+                new String[]{"pic","name","password"},new int[]{R.id.itemImage,R.id.itemName,R.id.itemPassword});
+        listView.setAdapter(simpleAdapter);
+
+        LayoutAnimationController lac=new LayoutAnimationController(AnimationUtils.loadAnimation(this, R.anim.activityin));
+        lac.setOrder(LayoutAnimationController.ORDER_NORMAL);
+        listView.setLayoutAnimation(lac);
+        listView.startLayoutAnimation();
+    }
+
+
+    private void listTest(){
+        for (int i = 0; i < 20; i++) {
+            Map<String, Object> map = new HashMap<String, Object>();
+            map.put("pic", R.mipmap.ic_launcher);
+            map.put("name", "xiamin" + i);
+            map.put("password",i+ "sss");
+            datalist.add(map);
+            Log.i("iii",map.toString());
+        }
+        simpleAdapter.notifyDataSetChanged();
+
+
     }
 
 }
