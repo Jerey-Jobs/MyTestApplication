40d67211b733f9eaf676498d087c52ae54acb9d4
Xiamin <610315802@qq.com>
2016-07-30 11:34:02 +0800

    主框架搭建

--- yiyiapp/src/main/java/com/example/yiyiapp/MainActivity.java
+++ yiyiapp/src/main/java/com/example/yiyiapp/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.yiyiapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void InitView()
+    {
+
+    }
+
+    private void checkInternet()
+    {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
