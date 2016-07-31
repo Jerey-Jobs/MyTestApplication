5670ace7ae6e81d4c1a633c033f38453a62ea5d4
Xiamin <610315802@qq.com>
2016-07-31 16:22:12 +0800

    接下来主要工作为：历史记录功能的开发，添加收藏列表功能的开发

---
+++ yiyiapp/src/main/java/com/example/yiyiapp/HistoryActivity.java
@@ -0,0 +1,16 @@
+package com.example.yiyiapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Xiamin on 2016/7/31.
+ */
+public class HistoryActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
