86f00451728f1b68912f864e690918faf49f20fa
Xiamin <610315802@qq.com>
2017-03-19 20:30:38 +0800

    增加Demo，如何将一个Activity放到系统的最近任务列表里，独立于宿主app任务卡之外

---
+++ Taskaffinity/src/main/java/com/jerey/taskaffinity/MainActivity.java
@@ -0,0 +1,23 @@
+package com.jerey.taskaffinity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+        //        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+    }
+}
